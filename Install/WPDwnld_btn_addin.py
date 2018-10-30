8c901b6ff917a7eb5be26fe4c7be6faf907b2444
Kerr D <dk2n16@soton.ac.uk>
2018-10-30 14:07:21 +0000

    Added logic to check for internet connection. Addin can't be tested until FTP is opened.

--- Install/WPDwnld_btn_addin.py
+++ Install/WPDwnld_btn_addin.py
@@ -1,5 +1,10 @@
 import arcpy
 import pythonaddins
+import os
+import urllib2
+
+rel_path = os.path.dirname(__file__)
+toolbox_path = os.path.join(rel_path, "WPdownload.pyt")
 
 class ButtonClass1(object):
     """Implementation for WPDwnld_btn_addin.button (Button)"""
@@ -7,4 +12,10 @@ class ButtonClass1(object):
         self.enabled = True
         self.checked = False
     def onClick(self):
-        pass
+        try:
+    		urllib2.urlopen("https://www.google.com/")
+    	except Exception as e:
+    		pythonaddins.MessageBox("Sorry, there is no internet connection.", "Connection error")
+    	else:
+        	pythonaddins.GPToolDialog(toolbox_path, "WPDownload") 
+            #This function results in the following error being printed to the ArcMap Python console: "TypeError: GPToolDialog() takes at most 1 argument (2 given)" - This is a bug known to ESRI and is logged under issue no NIM089253
